8e52bb3f6b1dc7bbb8fc7fef9bf9ea155233e7df
hniz <hammamzahran@gmail.com>
2023-07-24 02:26:49 +0300

    ManagerScript

---
+++ Memory-Gamee/Assets/Assets/Scripts/CardManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    public int pairAmount;
+    public Sprite[] spriteList;
+
+    float offset; // Offset between cards
+    public GameObject cardPrefab;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    
+}
