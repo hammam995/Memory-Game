2339429e0c769dde7bf461c406e6b780918f5dff
hniz <hammamzahran@gmail.com>
2023-07-23 22:26:19 +0300

    Card Script 1

---
+++ Memory-Gamee/Assets/Assets/Scripts/Card.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    int cardId;
+    public SpriteRenderer cardFront;
+    public Animator anim;
+
+    public void SetCard(int _id, Sprite _sprite)
+    {
+        cardId = _id;
+        cardFront.sprite = _sprite;
+    }
+}
