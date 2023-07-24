14589a4477194e32181fe873d630da182a4d3b5c
hniz <hammamzahran@gmail.com>
2023-07-24 11:16:47 +0300

    Card Manager Script2

--- Memory-Gamee/Assets/Assets/Scripts/CardManager.cs
+++ Memory-Gamee/Assets/Assets/Scripts/CardManager.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
-    public int pairAmount;
+    public int pairAmount; // if 4 then we will have 8 cards in  the field ;
     public Sprite[] spriteList;
 
-    float offset; // Offset between cards
+    float offset = 1.2f; // Offset between cards
     public GameObject cardPrefab;
 
 
 
     void Start()
     {
-        
+        CreatePlayerField();
     }
 
-    
+
+    void CreatePlayerField()
+    {
+        for ( int i = 0; i<pairAmount; i++)
+        {
+            for (int j = 0; j < 2; j++) // this loop to decide how many time we want to repeat the card
+            {
+
+                Vector3 pos = new Vector3(i * offset, 0, 0); // every card position
+                GameObject newCard = Instantiate(cardPrefab, pos, Quaternion.identity);
+                newCard.GetComponent<Card>().SetCard(i, spriteList[i]);
+            }
+        }
+    }
 }
