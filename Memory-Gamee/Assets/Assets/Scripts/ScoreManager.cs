de15b94b0cbd31173c19a256efbd72a4d2bc03d8
hniz <hammamzahran@gmail.com>
2023-07-26 15:22:13 +0300

    Coding the Timer Bar

--- Memory-Gamee/Assets/Assets/Scripts/ScoreManager.cs
+++ Memory-Gamee/Assets/Assets/Scripts/ScoreManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
 
     public int timeForLevelToComplete = 60;
+    public Image timeImage;
 
     void Start()
     {
@@ -20,8 +22,11 @@ public class ScoreManager : MonoBehaviour
         int tempTime = timeForLevelToComplete;
         while (tempTime > 0)
         {
-            tempTime--;
+            tempTime--; // current time
             yield return new WaitForSeconds(1);
+
+            timeImage.fillAmount = tempTime / (float)timeForLevelToComplete; // currentTime/maxTime
+            // fill anount (0,1) so we use /
         }
         // Game Over
         GameManager.instance.GameOver();
