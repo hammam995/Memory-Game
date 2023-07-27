b6465d8426633c14d7ca954f98514cfc2332460a
hniz <hammamzahran@gmail.com>
2023-07-28 01:54:39 +0300

    Button Script

---
+++ Memory-Gamee/Assets/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+
+
+    public void LoadScene(string sceneToload)
+    {
+        SceneManager.LoadScene(sceneToload);
+    }
+
+
+    public void Restartlevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
